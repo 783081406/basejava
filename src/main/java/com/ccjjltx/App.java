504548882883135abd49bca4d00505aae21e4762
783081406 <783081406ccj@gmail.com>
2017-02-12 16:46:04 +0800

    第二台电脑提交的数据，测试远程通道是否有问题

--- src/main/java/com/ccjjltx/App.java
+++ src/main/java/com/ccjjltx/App.java
@@ -7,5 +7,6 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Hello World!");
+        System.out.println("第二台电脑提交的数据测试是否有问题");
     }
 }
