0ecfdb2da928da20e222342a146bc0b76f823446
783081406 <783081406ccj@gmail.com>
2017-02-14 15:50:56 +0800

    新增Address.java

---
+++ src/main/java/com/ccjjltx/object/Address.java
@@ -0,0 +1,22 @@
+package com.ccjjltx.object;
+
+/**
+ * Created by ccj on 2017/2/14.
+ * @author ccj
+ * @version 1.0
+ */
+public class Address {
+    private String detail;
+
+    public  Address(String detail){
+        this.detail=detail;
+    }
+
+    public void setDetail(String detail){
+        this.detail=detail;
+    }
+
+    public String getDetail(){
+        return this.detail;
+    }
+}
