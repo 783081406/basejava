87eef24c05496f10b6a57877721a89d2a8c0db51
783081406 <783081406ccj@gmail.com>
2017-02-18 15:38:26 +0800

    新增StringBuilder概述

---
+++ src/main/java/com/ccjjltx/string/stringbuilder/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Created by ccj on 2017/2/18.
+ * StringBuilder类也代表字符串对象。实际上，StringBuilder和StringBuffer基本相似，构造器和方法也基本相同，
+ * 不同的是StringBuffer是线程安全的，而StringBuilder则没有实现线程安全功能，所以性能略高。
+ * 通常情况下，如果需要创建一个内容可变的字符串对象，则可以优先考虑使用StringBuilder类
+ */
+package com.ccjjltx.string.stringbuilder;
