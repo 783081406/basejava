9693d710b1fd8797c578a94fd8b9dc84fc4c05cc
783081406 <783081406ccj@gmail.com>
2017-02-18 15:29:29 +0800

    新增String概述

---
+++ src/main/java/com/ccjjltx/string/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Created by ccj on 2017/2/18.
+ * String类包括的方法用于检查序列的单个字符串、比较字符串、搜索字符串、创建字符串副本并将所有字符全部转换为大写或小写、大小写映射基于Character类指定的Unicode标准版
+ * String是不可变，即一旦一个String对象被创建以后，包含在这个对象中的字符串是不能改变的，直到对象被销毁
+ */
+package com.ccjjltx.string;
