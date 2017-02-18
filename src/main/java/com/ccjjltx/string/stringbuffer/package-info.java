d8af19d00ecacc7bdff8d556d2467cc11d5bceca
783081406 <783081406ccj@gmail.com>
2017-02-18 15:34:19 +0800

    新增StringBuffer概述

---
+++ src/main/java/com/ccjjltx/string/stringbuffer/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Created by ccj on 2017/2/18.
+ *
+ * StringBuffer对象则代表一个字符序列可变的字符串。当一个StringBuffer被创建以后，
+ * 通过StringBuffer提供的append()、insert()、reverse()、setCharAt()、setLength()等方法可以改变这个字符串对象的字符序列。
+ * 一旦通过StringBuffer生成了最终想要的字符串，就可以调用它的toString()方法将其转换为一个String对象
+ */
+package com.ccjjltx.string.stringbuffer;
