9edf28bb61cd593e4d74944d89e287700892b087
783081406 <783081406ccj@gmail.com>
2017-02-26 23:13:38 +0800

    新增ThreadLocalRandom概述

---
+++ src/main/java/com/ccjjltx/random/threadlocalrandom/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Created by ccjjltx on 2017/2/26.
+ * 多线程情况下建议使用ThreadLocalRandom
+ * 不能直接实例化，第一次使用要调用其静态方法current()
+ * @author ccj
+ * @version 1.0
+ */
+package com.ccjjltx.random.threadlocalrandom;
