6ce9ae67d0636617be7fc364cb318b4affcd1e6d
783081406 <783081406ccj@gmail.com>
2017-02-26 19:42:44 +0800

    新增Math解析

---
+++ src/main/java/com/ccjjltx/math/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Created by ccjjltx on 2017/2/26.
+ * Math类是一个工具类，他的构造器被定义成private的，因此无法创建Math类的对象；
+ * Math类中的所有方法都是类方法
+ * Math除了提供了大量静态方法之外，还提供两个类变量，PI和E
+ * @author ccj
+ * @version 1.0
+ */
+package com.ccjjltx.math;
