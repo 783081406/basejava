a63deddd68c29f4cad4087fbf38c1002aa17f0b8
783081406 <783081406ccj@gmail.com>
2017-03-01 14:19:59 +0800

    新增DescAgeComparator.java实现Comparator接口,按照age降序排序

--- src/main/java/com/ccjjltx/comparable/DescAgeComparator.java
+++ src/main/java/com/ccjjltx/comparable/DescAgeComparator.java
@@ -4,14 +4,14 @@ import java.util.Comparator;
 
 /**
  * Created by ccjjltx on 2017/3/1.
- * AscAgeComparator比较器, 它是“Person的age的升序比较器
+ * DescAgeComparator比较器,它是“Person的age的降序比较器
  *
  * @author ccj
  * @version 1.0
  */
-public class AscAgeComparator implements Comparator<Person> {
+public class DescAgeComparator implements Comparator<Person> {
     @Override
     public int compare(Person o1, Person o2) {
-        return o1.getAge() - o2.getAge();
+        return o2.getAge() - o1.getAge();
     }
 }
