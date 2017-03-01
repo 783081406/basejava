065704aec9a92abc2a5aef991d2b308302b1d414
783081406 <783081406ccj@gmail.com>
2017-03-01 15:18:57 +0800

    prective为练习包,新增联系需求

---
+++ src/main/java/com/ccjjltx/comparable/prectice/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * Created by ccjjltx on 2017/3/1.
+ * 需求：
+ * 有个Student类，包括姓名、性别、年龄属性：现在有个需求，需要对Person进行以下规则的排序
+ * 1.按拼音
+ * 2.同名按性别，女性排前
+ * 3.姓名、性别相同，年龄小排前
+ * @author ccj
+ * @version 1.0
+ */
+package com.ccjjltx.comparable.prectice;
