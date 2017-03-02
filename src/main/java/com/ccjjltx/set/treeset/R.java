cac2c14470e39a43148f0d02c6842c742f7398a0
783081406 <783081406ccj@gmail.com>
2017-03-02 14:28:23 +0800

    修改R.java

--- src/main/java/com/ccjjltx/set/treeset/R.java
+++ src/main/java/com/ccjjltx/set/treeset/R.java
@@ -2,34 +2,44 @@ package com.ccjjltx.set.treeset;
 
 /**
  * Created by ccjjltx on 2017/3/2.
+ *
  * @author ccj
  * @version 1.0
  */
-public class R implements Comparable<R>{
+public class R implements Comparable<R> {
     private int count;
-    public R(int count){
-        this.count=count;
+
+    public R(int count) {
+        this.count = count;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
     }
 
     @Override
-    public String toString(){
-        return "R[count:"+count+"]";
+    public String toString() {
+        return "R[count:" + count + "]";
     }
 
     @Override
-    public boolean equals(Object obj){
-        if(this==obj){
+    public boolean equals(Object obj) {
+        if (this == obj) {
             return true;
         }
-        if(obj!=null&&obj.getClass()==R.class){
-            R r=(R)obj;
-            return r.count==this.count;
+        if (obj != null && obj.getClass() == R.class) {
+            R r = (R) obj;
+            return r.count == this.count;
         }
         return false;
     }
 
     @Override
     public int compareTo(R r) {
-        return count>r.count?1:count<r.count?-1:0;
+        return count > r.count ? 1 : count < r.count ? -1 : 0;
     }
 }
