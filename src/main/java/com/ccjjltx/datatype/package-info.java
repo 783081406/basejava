a63296447eb7f7a37732cf61e6f560109f84a47e
783081406 <783081406ccj@gmail.com>
2017-03-03 16:40:45 +0800

    package-info

---
+++ src/main/java/com/ccjjltx/datatype/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Created by ccjjltx on 2017/3/3.
+ * 基本数据类型
+ *
+ * @author ccj
+ * @version 1.0
+ */
+package com.ccjjltx.datatype;
---
+++ src/main/java/com/ccjjltx/flowcontrol/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Created by ccjjltx on 2017/3/3.
+ * 流程控制
+ *
+ * @author ccj
+ * @version 1.0
+ */
+package com.ccjjltx.flowcontrol;
