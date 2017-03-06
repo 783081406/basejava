b7e55e0e78044f110e0aa2f8ded3f0d87a66fbce
783081406 <783081406ccj@gmail.com>
2017-03-06 15:17:31 +0800

    修改set包结构进入collection包下

--- src/main/java/com/ccjjltx/set/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * Created by ccjjltx on 2017/2/28.
- * Set集合和Collection基本相同，Set集合不允许包含相同的元素，如果试图把两个相同的元素加入同一个Set集合中
- * 则添加操作失败，add()方法返回false，切新元素不会被加入
- * @author ccj
- * @version 1.0
- */
-package com.ccjjltx.set;
