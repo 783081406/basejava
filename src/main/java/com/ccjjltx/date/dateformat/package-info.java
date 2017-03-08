ca447de9daafac8c432c1844527f3baeb30e28f5
783081406 <783081406ccj@gmail.com>
2017-03-08 22:39:30 +0800

    DateFormat解析

---
+++ src/main/java/com/ccjjltx/date/dateformat/package-info.java
@@ -0,0 +1,30 @@
+/**
+ * Created by ccjjltx on 2017/3/8.
+ * FULL,LONG,MEDIUM,SHORT
+ * DateFormat是抽象方法，用内置的实例化方法一般返回SimpleDateFormat：
+ * getInstance():
+ * getDateInstance():
+ * getTimeInstance():
+ * getDateTimeInstance():
+ * 方法：
+ * Object clone():
+ * boolean equals(Object obj):
+ * String format(Date date):格式化Date转String类型
+ * StringBuffer format(Date date,StringBuffer toAppendTo,FieldPosition fieldPosition):
+ * StringBuffer format(Object obj,StringBuffer toAppendTo,FieldPosition fieldPosition):
+ * Calendar getCalendar():获取与此格式相关的时间日期格式
+ * NumberFormat getNumberFormat():
+ * TimeZone getTimeZone():
+ * int hashCode():
+ * boolean isLenient():
+ * Date parse(String source):解析String转Date
+ * Date parse(String source,ParsePosition pos):
+ * void setCalendar(Calendar newCalendar):
+ * void setLenient(boolean lenient):
+ * void setNumberFormat(NumberFormat newNumberFormat):
+ * void setTimeZone(TimeZone zone):
+ *
+ * @author ccj
+ * @version 1.0
+ */
+package com.ccjjltx.date.dateformat;
