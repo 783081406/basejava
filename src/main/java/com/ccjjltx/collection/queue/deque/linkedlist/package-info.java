aa6459ee3d0039727f5d7a4c6ff95c29e6ce2914
783081406 <783081406ccj@gmail.com>
2017-03-11 19:53:35 +0800

    LinkedList概述

---
+++ src/main/java/com/ccjjltx/collection/queue/deque/linkedlist/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * Created by ccjjltx on 2017/3/11.
+ * LinkedList类是List接口的实现类-这意味着他是一个List集合，可以根据索引来随机访问集合中的元素。
+ * 除此之外，LinkedList还实现了Deque接口，可以被当成双端口队列来使用，因此既可以被当成“栈”来使用
+ * 也可以当成队列来使用
+ *
+ * @author ccj
+ * @version 1.0
+ */
+package com.ccjjltx.collection.queue.deque.linkedlist;
