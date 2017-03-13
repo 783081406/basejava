bde8eb41e49ae8a57732e4a9a0bbfabeb67ab859
783081406 <783081406ccj@gmail.com>
2017-03-13 11:35:57 +0800

    HashSet里面使用自定义的对象，重写equals和hashCode()方法

--- src/main/java/com/ccjjltx/collection/set/hashset/HashSetTest.java
+++ src/main/java/com/ccjjltx/collection/set/hashset/HashSetTest.java
@@ -16,7 +16,7 @@ public class HashSetTest {
         books.add(new A());
         books.add(new B());
         books.add(new B());
-        //只有五个结果，下面两个实际上会在相同位置用链接结构保存多个对象；而HashSet访问集合元素时也是根据元素的hashCode值来快速定位
+        //只有五个结果，下面两个实际上会在相同位置用链式结构保存多个对象；而HashSet访问集合元素时也是根据元素的hashCode值来快速定位
         //如果HashSet中两个以上的元素具有相同的hashCode值，将会导致性能下降
         books.add(new C());
         books.add(new C());
