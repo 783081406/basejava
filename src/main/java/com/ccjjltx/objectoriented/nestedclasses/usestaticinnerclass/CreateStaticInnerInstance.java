cd56c30ec898ee02908650a7b4ad62351b4720f6
783081406 <783081406ccj@gmail.com>
2017-03-18 13:33:37 +0800

    调用静态内部类

---
+++ src/main/java/com/ccjjltx/objectoriented/nestedclasses/usestaticinnerclass/CreateStaticInnerInstance.java
@@ -0,0 +1,13 @@
+package com.ccjjltx.objectoriented.nestedclasses.usestaticinnerclass;
+
+/**
+ * Created by ccjjltx on 2017/3/18.
+ *
+ * @author ccj
+ * @version 1.0
+ */
+public class CreateStaticInnerInstance {
+    public static void main(String[] args) {
+        StaticOut.StaticIn in=new StaticOut.StaticIn();
+    }
+}
