342e77ffa83bcc8654f9d36c84183618c1c80cd1
783081406 <783081406ccj@gmail.com>
2017-03-18 13:38:20 +0800

    继承静态内部类

---
+++ src/main/java/com/ccjjltx/objectoriented/nestedclasses/usestaticinnerclass/StaticSubClass.java
@@ -0,0 +1,12 @@
+package com.ccjjltx.objectoriented.nestedclasses.usestaticinnerclass;
+
+/**
+ * Created by ccjjltx on 2017/3/18.
+ * 继承静态内部类
+ * 因为调用内部类的构造器无需使用外部类，所以创建静态内部类的子类比较简单
+ *
+ * @author ccj
+ * @version 1.0
+ */
+public class StaticSubClass extends StaticOut.StaticIn {
+}
