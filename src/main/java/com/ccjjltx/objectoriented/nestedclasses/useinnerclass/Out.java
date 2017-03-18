a3ff609f821b77d35313c1433dfda4d29cf10d20
783081406 <783081406ccj@gmail.com>
2017-03-18 13:17:09 +0800

    含非静态内部类In

---
+++ src/main/java/com/ccjjltx/objectoriented/nestedclasses/useinnerclass/Out.java
@@ -0,0 +1,15 @@
+package com.ccjjltx.objectoriented.nestedclasses.useinnerclass;
+
+/**
+ * Created by ccjjltx on 2017/3/18.
+ *
+ * @author ccj
+ * @version 1.0
+ */
+public class Out {
+    class In {
+        public In(String msg) {
+            System.out.println(msg);
+        }
+    }
+}
