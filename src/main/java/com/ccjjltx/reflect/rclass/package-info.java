68a667ff2e7d45b8c4262b91059e60bdcb373fc0
783081406 <783081406ccj@gmail.com>
2017-03-19 15:57:35 +0800

    Class三种获得方式

---
+++ src/main/java/com/ccjjltx/reflect/rclass/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * Created by ccjjltx on 2017/3/19.
+ * 首先要生成Class对象，然后再生成Instance
+ * 其中Class对象的生成方式主要有三种：
+ * 1.Class.forName("类名字符串"):(类名字符串都必须要全称，包名+类名）
+ * 2.类名.class
+ * 3.实例对象.getClass()
+ *
+ * @author ccj
+ * @version 1.0
+ */
+package com.ccjjltx.reflect.rclass;
