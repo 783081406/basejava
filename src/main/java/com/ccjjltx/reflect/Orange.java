38c98a4bfa898ce82bd4012238609cd3930e97b5
783081406 <783081406ccj@gmail.com>
2017-03-19 16:05:18 +0800

    Fruit系列

---
+++ src/main/java/com/ccjjltx/reflect/Orange.java
@@ -0,0 +1,14 @@
+package com.ccjjltx.reflect;
+
+/**
+ * Created by ccjjltx on 2017/3/19.
+ *
+ * @author ccj
+ * @version 1.0
+ */
+public class Orange implements Fruit {
+    @Override
+    public void eat() {
+        System.out.println("Orange");
+    }
+}
