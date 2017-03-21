ec7cc6f853a5b5c8c9d8f0134e5d30f6052af42a
783081406 <783081406ccj@gmail.com>
2017-03-21 17:43:23 +0800

    定义一个接口

---
+++ src/main/java/com/ccjjltx/objectoriented/renum/rimplements/GenderDesc.java
@@ -0,0 +1,12 @@
+package com.ccjjltx.objectoriented.renum.rimplements;
+
+/**
+ * Created by ccjjltx on 2017/3/21.
+ * 接口
+ *
+ * @author ccj
+ * @version 1.0
+ */
+public interface GenderDesc {
+    void info();
+}
