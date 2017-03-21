a66581ceb225208c9dea1cf897f6b63255210f69
783081406 <783081406ccj@gmail.com>
2017-03-21 16:12:45 +0800

    季节枚举类，定义四个枚举

---
+++ src/main/java/com/ccjjltx/objectoriented/renum/season/SeasonEnum.java
@@ -0,0 +1,13 @@
+package com.ccjjltx.objectoriented.renum.season;
+
+/**
+ * Created by ccjjltx on 2017/3/21.
+ *
+ * @author ccj
+ * @version 1.0
+ */
+public enum SeasonEnum {
+    //在第一行显式罗列出，否则这个枚举类永远都不能产生实例化
+    //系统自动换添加public static final修饰
+    SPRING, SUMMER, FALL, WINTER;
+}
