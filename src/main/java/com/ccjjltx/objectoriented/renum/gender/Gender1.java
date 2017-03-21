b05bc1a60400784d19f97afbd17e2ea4a5d1c08d
783081406 <783081406ccj@gmail.com>
2017-03-21 17:17:38 +0800

    未修改前的Gender枚举类

---
+++ src/main/java/com/ccjjltx/objectoriented/renum/gender/Gender1.java
@@ -0,0 +1,34 @@
+package com.ccjjltx.objectoriented.renum.gender;
+
+
+/**
+ * Created by ccjjltx on 2017/3/21.
+ *
+ * @author ccj
+ * @version 1.0
+ */
+public enum Gender1 {
+    MALE, FEMALE;
+    private String name;
+
+    public void setName(String name) {
+        switch (this) {
+            case MALE:
+                if (name.equals("男")) {
+                    this.name = name;
+                } else {
+                    System.out.println("参数化错误");
+                    return;
+                }
+                break;
+            default:
+                if (name.equals("女")) {
+                    this.name = name;
+                } else {
+                    System.out.println("参数化错误");
+                    return;
+                }
+                break;
+        }
+    }
+}
