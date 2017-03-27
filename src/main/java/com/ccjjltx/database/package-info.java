4360775c73957727e5cb2cd0b55c9768aa6b7211
783081406 <783081406ccj@gmail.com>
2017-03-27 13:46:29 +0800

    explain

---
+++ src/main/java/com/ccjjltx/database/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Created by ccjjltx on 2017/3/27.
+ *
+ * @author ccj
+ * @version 1.0
+ */
+package com.ccjjltx.database;
---
+++ src/main/java/com/ccjjltx/database/statement/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * Created by ccjjltx on 2017/3/27.
+ * statement主要有三个函数：
+ * ResultSet executeQuery():
+ * int executeUpdate():
+ * boolean execute():
+ *
+ * @author ccj
+ * @version 1.0
+ */
+package com.ccjjltx.database.statement;
