46b05ce2d21bcfae81336df8955e5ef7c96974f8
783081406 <783081406ccj@gmail.com>
2017-04-04 17:52:28 +0800

    java.time.format概述

---
+++ src/main/java/com/ccjjltx/date/j8/format/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * Created by ccjjltx on 2017/4/4.
+ * java.time.format包是专门用来格式化输出输入时间/日期的。这个包围绕DateTimeFormatter类和它的辅助创建类DateTimeFormatterBuilder展开。静态方法ofPattern(Charqueue)和DateTimeFormatter中的常量是最通用的创建格式化器的方式
+ * <p>
+ * 常用ISO格式常量，如ISO_LOCAL_DATE
+ * 字母模式，如ofPattern(“dd/MM/uuuu”)
+ * 本地化样式，如ofLocalizedDate(FormatStyle.MEDIUM)
+ *
+ * @author ccj
+ * @version 1.0
+ */
+package com.ccjjltx.date.j8.format;
