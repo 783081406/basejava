54cf35dea1f12445c357fc0ada8374e251097e21
783081406 <783081406ccj@gmail.com>
2017-04-04 14:22:56 +0800

    MonthDay概述

---
+++ src/main/java/com/ccjjltx/date/j8/monthday/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Created by ccjjltx on 2017/4/4.
+ * 该类仅代表月日，例如--04-12.该类提供了静态的now()方法来获取当前月日，
+ * 也提供了静态的now(Clock clock)方法来获取clock对应的月日
+ *
+ * @author ccj
+ * @version 1.0
+ */
+package com.ccjjltx.date.j8.monthday;
