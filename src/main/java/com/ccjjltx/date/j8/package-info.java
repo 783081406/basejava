63752cff99bb65b3c3e691d6ad5290fd52c229f1
783081406 <783081406ccj@gmail.com>
2017-04-04 15:05:58 +0800

    java8时间概述

--- src/main/java/com/ccjjltx/date/j8/package-info.java
+++ src/main/java/com/ccjjltx/date/j8/package-info.java
@@ -1,5 +1,18 @@
 /**
  * Created by ccjjltx on 2017/4/3.
+ * Clock
+ * Duration:
+ * Instant:
+ * LocalDate:
+ * LocalTime:
+ * LocalDateTime:
+ * MonthDay:
+ * Year:
+ * YearMonth:
+ * ZonedDateTime:
+ * ZoneId:
+ * DayOfWeek:
+ * Month:
  *
  * @author ccj
  * @version 1.0
