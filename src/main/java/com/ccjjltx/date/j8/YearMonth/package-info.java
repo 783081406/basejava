cabc07210129d605112495a7049f0c49eec932e7
783081406 <783081406ccj@gmail.com>
2017-04-04 14:32:57 +0800

    YearMonth概述

---
+++ src/main/java/com/ccjjltx/date/j8/YearMonth/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * Created by ccjjltx on 2017/4/4.
+ * 该类仅代表年月，例如2014-04.该类提供了静态的now()方法来获取当前年月
+ * 也提供了静态的now(Clock clock)方法来获取clock对应的年月。
+ * 除此之外还提供了minusXxx()方法在当前年月基础上减去几年，几月，也提供了
+ * 也提供了plusXxx()方法在当前年月基础上加上几年、几月
+ *
+ * @author ccj
+ * @version 1.0
+ */
+package com.ccjjltx.date.j8.YearMonth;
