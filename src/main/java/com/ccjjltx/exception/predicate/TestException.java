01f0dff24a7692be0b662606084cf34621d44b34
783081406 <783081406ccj@gmail.com>
2017-04-09 15:31:37 +0800

    Predicate  TestException

---
+++ src/main/java/com/ccjjltx/exception/predicate/TestException.java
@@ -0,0 +1,21 @@
+package com.ccjjltx.exception.predicate;
+
+/**
+ * Created by ccjjltx on 2017/4/9.
+ *
+ * @author ccj
+ * @version 1.0
+ */
+public class TestException {
+    public static void main(String[] args) {
+        try {
+            testException();
+        } catch (MyException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void testException() throws MyException{
+        throw new MyException("MyException");
+    }
+}
