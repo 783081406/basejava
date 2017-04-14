ed366dacdf2209e5f5b2549e3df6fb431062e7d5
783081406 <783081406ccj@gmail.com>
2017-04-14 14:39:04 +0800

    概述

---
+++ src/main/java/com/ccjjltx/io/convertflow/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * Created by ccjjltx on 2017/4/14.
+ * 转换流(字节流转换到字符流)
+ * InputStreamReader:继承类Reader
+ * OutputStreamWriter:继承类Writer
+ *
+ * @author ccj
+ * @version 1.0
+ */
+package com.ccjjltx.io.convertflow;
