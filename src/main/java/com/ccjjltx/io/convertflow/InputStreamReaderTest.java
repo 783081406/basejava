f9aa4b4c90b9994ea1d0945c99157b02fce05257
783081406 <783081406ccj@gmail.com>
2017-04-14 14:42:33 +0800

    InputStreamReader      practice

--- src/main/java/com/ccjjltx/io/convertflow/InputStreamReaderTest.java
+++ src/main/java/com/ccjjltx/io/convertflow/InputStreamReaderTest.java
@@ -8,6 +8,8 @@ import java.io.InputStreamReader;
  * Created by ccjjltx on 2017/4/14.
  * 字节流转换成字符流（节点流）
  * 把节点流转换成包装流
+ * BufferedReader具有一个readLine()方法，可以非常方便地一次读取一行内容，所以经常把读取文本内容的输入流包装成BufferedReader,
+ * 用于方便地读取输入流的文本内容
  *
  * @author ccj
  * @version 1.0
