197031f7c8bc761b24c0ec3352ff821459d99077
783081406 <783081406ccj@gmail.com>
2017-04-18 20:30:37 +0800

    nio2   概述

---
+++ src/main/java/com/ccjjltx/io/nio2/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * Created by ccjjltx on 2017/4/18.
+ * 改进：
+ * 1.提供了全面的文件IO和文件系统访问支持（Java7新增的java.nio.file包及各种子包）
+ * 2.基于异步Channel的IO（java.nio.channels包下新增加了多个以Asynchronous开头的Channel接口和类）
+ * <p>
+ * Path接口代表一个平台无关的平台路径。
+ *
+ * @author ccj
+ * @version 1.0
+ */
+package com.ccjjltx.io.nio2;
