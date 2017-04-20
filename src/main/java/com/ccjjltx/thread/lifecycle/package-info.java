460e8bf21edb7005c4205cfed89a2279f75d9d6e
783081406 <783081406ccj@gmail.com>
2017-04-20 19:57:56 +0800

    生命周期概述

---
+++ src/main/java/com/ccjjltx/thread/lifecycle/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Created by ccjjltx on 2017/4/20.
+ * 测试某个线程是否已经死亡，可以调用线程对象的isAlive()方法，当线程处于就绪、运行、阻塞三种状态时该方法将返回true；
+ * 但线程处于新疆、死亡两种状态时，该方法返回false
+ *
+ * @author ccj
+ * @version 1.0
+ */
+package com.ccjjltx.thread.lifecycle;
