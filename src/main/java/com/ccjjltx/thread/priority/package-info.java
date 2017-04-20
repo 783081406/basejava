ad2e15900aebcf842c4bbcf96b653cc5773f71e9
783081406 <783081406ccj@gmail.com>
2017-04-20 23:20:49 +0800

    Priority     概述

--- src/main/java/com/ccjjltx/thread/priority/package-info.java
+++ src/main/java/com/ccjjltx/thread/priority/package-info.java
@@ -9,6 +9,9 @@
  * Min_PRIORITY:其值是1
  * NORM_PRIORITY:其值是5
  * 也可以是一个整数1~10
+ * <p>
+ * 不同操作系统上的优先级并不相同，而且也不能很好地和Java的10个优先级对象，例如Windows2000仅提供了7个优先级。因此应该尽量避免直接为
+ * 线程指定优先级，而应该使用MAX_PRIORITY,MIN_PRIORITY和NORM_PRIORITY三个静态常量设置优先级，这样才可以保证程序具有最后的可移植性。
  *
  * @author ccj
  * @version 1.0
