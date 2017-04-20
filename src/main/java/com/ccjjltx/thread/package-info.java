0f9cebc460844913ada344b1b0babf5b730bf1a8
783081406 <783081406ccj@gmail.com>
2017-04-20 16:06:46 +0800

    创建线程三种方法

--- src/main/java/com/ccjjltx/thread/package-info.java
+++ src/main/java/com/ccjjltx/thread/package-info.java
@@ -11,6 +11,26 @@
  * 线程是独立运行的，它并不知道进程中是否还有其他线程存在。线程的执行是抢占式的，也就是说当前运行的线程在任何时候都可能被挂起，一边另外一个线程很可以运行
  * <p>
  * 一个线程可以创建和撤销另一个线程，同一个进程中的多个线程之间可以并发执行。
+ * <p>
+ * 创建线程的三种方法：
+ * 1.继承Thread
+ * 2.继承Runnable
+ * 3.Callable接口（可以看作是Runnable接口的增强版，Callable接口提供了call()方法比run()方法功能更强大）
+ * <p>
+ * Future接口下的几个方法：
+ * boolean cancel(boolean myInterruptIfRunning):试图取消该Future里关联的Callable任务
+ * V get():返回Callable任务里call()方法的返回值。调用该方法将导致程序阻塞，必须等到子线程结束后才会得到返回值
+ * V get(long timeout,TimeUnit unit):返回Callable任务里call()方法的返回值。该方法让程序最多阻塞timeout和unit指定的时间，如果经过
+ * 指定时间后Callable任务依然没有返回值，将会抛出TimeoutException异常
+ * boolean isCancelled():如果在Callable任务正常完成前被取消，则返回true
+ * boolean isDone():如果Callable任务已完成，则返回true
+ * <p>
+ * 创建并启动有返回值的线程的步骤：
+ * 1.创建Callable接口的实现类，并实现call()方法，该call()方法，该call()方法将作为线程执行体，且该call()方法有返回值，再创建Callable实现类的实例
+ * 也可以直接使用Lambda表达式创建Callable对象
+ * 2.使用FutureTask类来包装Callable对象，该FutureTask对象封装了该Callable对象的call()方法的返回值
+ * 3.使用FutureTask对象作为Thread对象的target创建并启动新线程
+ * 4.调用FutureTask对象的get()方法来获取子线程执行结束后的返回值
  *
  * @author ccj
  * @version 1.0
