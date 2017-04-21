ee75016f7c22b82710585c0c4f1f742466613223
783081406 <783081406ccj@gmail.com>
2017-04-21 17:30:20 +0800

    lock  概述

---
+++ src/main/java/com/ccjjltx/thread/lock/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * Created by ccjjltx on 2017/4/21.
+ * Lock、ReadWriteLock两个根接口
+ * Lock实现类：ReentrantLock（可重入锁)实现类。
+ * ReadWriteLock实现类:ReentrantReadWriteLock实现类(提供了Writing、ReadingOptimistic、Reading)
+ * StampedLock类代替传统的ReentrantReadWriteLock
+ *
+ * @author ccj
+ * @version 1.0
+ */
+package com.ccjjltx.thread.lock;
