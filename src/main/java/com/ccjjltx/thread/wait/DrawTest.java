1eefe2917a266f058c31d7d426c9954f387d2219
783081406 <783081406ccj@gmail.com>
2017-04-22 11:35:13 +0800

    取钱线程

---
+++ src/main/java/com/ccjjltx/thread/wait/DrawTest.java
@@ -0,0 +1,18 @@
+package com.ccjjltx.thread.wait;
+
+/**
+ * Created by ccjjltx on 2017/4/22.
+ *
+ * @author ccj
+ * @version 1.0
+ */
+public class DrawTest {
+    public static void main(String[] args) {
+        //创建一个账户
+        Account acct = new Account("1234567", 0);
+        new DrawThread("取钱者", acct, 800).start();
+        new DepositThread("存款者甲", acct, 800).start();
+        new DepositThread("存款者乙", acct, 800).start();
+        new DepositThread("存款者丙", acct, 800).start();
+    }
+}
