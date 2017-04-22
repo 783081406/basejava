69f5f70e0a966460dde01da3b9d0a28e09c2cd15
783081406 <783081406ccj@gmail.com>
2017-04-22 22:36:40 +0800

    ExHandler   test

--- src/main/java/com/ccjjltx/thread/threadgroup/exhandler/ExHandler.java
+++ src/main/java/com/ccjjltx/thread/threadgroup/exhandler/ExHandler.java
@@ -13,5 +13,8 @@ public class ExHandler {
         int a = 5 / 0;
         System.out.println("程序正常结束!");
         //Thread[main,5,main]线程出现了异常:java.lang.ArithmeticException: / by zero
+        //程序依然不会正确结束
+        //这说明异常处理器与通过catch捕获异常是不同的——当使用catch捕获异常时，异常不会向上传播给上一级调用者;但使用异常处理器对
+        //异常依然会传播给上一级调用者
     }
 }
