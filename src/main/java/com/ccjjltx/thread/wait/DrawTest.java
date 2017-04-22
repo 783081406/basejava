3d767a8d20fa8273722948c13e50ae8b533b2bcb
783081406 <783081406ccj@gmail.com>
2017-04-22 11:36:50 +0800

    Main   test

--- src/main/java/com/ccjjltx/thread/wait/DrawTest.java
+++ src/main/java/com/ccjjltx/thread/wait/DrawTest.java
@@ -14,5 +14,6 @@ public class DrawTest {
         new DepositThread("存款者甲", acct, 800).start();
         new DepositThread("存款者乙", acct, 800).start();
         new DepositThread("存款者丙", acct, 800).start();
+        //观察1变化
     }
 }
