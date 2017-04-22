cb7c73a104000d6b93bad7b01e780561d99a920e
783081406 <783081406ccj@gmail.com>
2017-04-22 15:24:34 +0800

    DepositThread

---
+++ src/main/java/com/ccjjltx/thread/condition/DepositThread.java
@@ -0,0 +1,29 @@
+package com.ccjjltx.thread.condition;
+
+/**
+ * Created by ccjjltx on 2017/4/22.
+ *
+ * @author ccj
+ * @version 1.0
+ */
+public class DepositThread extends Thread {
+    // 模拟用户账户
+    private Account account;
+    // 当前取钱线程所希望存款的钱数
+    private double depositAmount;
+
+    public DepositThread(String name, Account account
+            , double depositAmount) {
+        super(name);
+        this.account = account;
+        this.depositAmount = depositAmount;
+    }
+
+    // 重复100次执行存款操作
+    public void run() {
+        for (int i = 0; i < 100; i++) {
+            account.deposit(depositAmount);
+        }
+    }
+}
+
