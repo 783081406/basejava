628d800ae72ec1a3ceccc2916e1cb2381efdd6ba
783081406 <783081406ccj@gmail.com>
2017-04-24 09:46:18 +0800

    URLConnection 概述！

--- src/main/java/com/ccjjltx/network/getpost/package-info.java
+++ src/main/java/com/ccjjltx/network/getpost/package-info.java
@@ -15,9 +15,24 @@
  * setDoInput():设置该URLConnection的doInput请求头字段的值
  * setDoOutput():设置该URLConnection的doOutput请求头字段的值
  * setIfModifiedSince():设置该URLConnection的ifModifiedSince请求头部字段的值
- *
+ * <p>
  * 设置或增加通用头字段
- * setRequestProperty(String key,String value):
+ * setRequestProperty(String key,String value):设置该URLConnection的key请求头字段的值为value.如:conn.setRequestProperty("accept","/")
+ * addRequestProperty(String key,String value):为该URLConnection的key请求头字段增加value值,该方法并不会覆盖原来请求头字段的值，而是将
+ * 新值追加到原请求头字段中
+ * <p>
+ * 当远程资源可用之后，程序可以使用下面方法来访问头部字段和内容
+ * Object getContent():获取该URLConnection的内容
+ * String getHeaderField(String name):获取制定响应头字段的值
+ * getInputStream():返回该URLConnection对应的输入流，用于获取URLConnection相应的内容
+ * getOutputStream():返回该URLConnection对应的输出流，用于想URLConnection发送请求参数
+ * getHeaderField()方法用于根据响应头字段来返回对应的值.而某些头字段由于经常需要访问，所以Java提供下面几个方法来访问特定相应头字段的值
+ * getContentEncoding():获取content-encoding响应头字段的值
+ * getContentLength():获取content-length响应头字段的值
+ * getContentType():获取content-type响应头字段的值
+ * getDate():获取date响应头字段的值
+ * getExpiration();获取expires响应头字段的值
+ * getLastModified():获取last-modified响应头字段的值。
  *
  * @author ccj
  * @version 1.0
